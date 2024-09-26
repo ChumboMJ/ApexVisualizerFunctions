8ed5910835f09d8a6d1452ec7473339019def7bd
tim-forste <tforste@gmail.com>
2024-09-25 17:13:55 -0700

    Simple setup to return an address from the ApexDB

--- ApexVisualizerFunctions/PostRaceResults.cs
+++ ApexVisualizerFunctions/PostRaceResults.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace ApexVisualizerFunctions
@@ -8,17 +10,42 @@ namespace ApexVisualizerFunctions
     public class PostRaceResults
     {
         private readonly ILogger<PostRaceResults> _logger;
+        private readonly string _connectionString;
 
-        public PostRaceResults(ILogger<PostRaceResults> logger)
+        public PostRaceResults(ILogger<PostRaceResults> logger, IConfiguration configuration)
         {
+            logger.LogInformation("Configuration loaded: {config}", configuration.ToString());
             _logger = logger;
+
+            //TODO: This works, but need to add a SqlConnection section to local.settings.json so we can use "GetConnectioString" instead
+            _connectionString = configuration.GetSection("SqlConnection")?.Value ?? throw new ArgumentNullException(nameof(configuration), "Connection string 'SqlConnection' not found.");
+
         }
 
         [Function("PostRaceResults")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT TOP (1) * FROM [SalesLT].[Address]", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // Process the data (example)
+                    var result = reader["AddressLine1"].ToString();
+                    return new OkObjectResult($"Race result: {result}");
+                }
+                else
+                {
+                    return new NotFoundResult();
+                }
+            }
+
+            //return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
 }
--- ApexVisualizerFunctions/Program.cs
+++ ApexVisualizerFunctions/Program.cs
@@ -1,14 +1,41 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureAppConfiguration((context, config) =>
     {
+        // Load configuration from local.settings.json and environment variables
+        config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+              .AddEnvironmentVariables();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        // Register Application Insights
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+
+        // Register configuration
+        services.AddSingleton<IConfiguration>(context.Configuration);
+
+        // Add logging to verify configuration
+        services.AddLogging(loggingBuilder =>
+        {
+            loggingBuilder.AddConsole();
+            loggingBuilder.AddDebug();
+        });
     })
     .Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+if (configuration is IConfigurationRoot configurationRoot)
+{
+    logger.LogInformation("Configuration loaded: {config}", configurationRoot.GetDebugView());
+}
 
 host.Run();
