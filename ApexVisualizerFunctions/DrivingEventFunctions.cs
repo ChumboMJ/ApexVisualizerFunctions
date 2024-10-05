1e764e739f303bcb5648567aadd459c20929ddcf
tim-forste <tforste@gmail.com>
2024-10-05 11:00:16 -0700

    Begin work on Cosmos Driving Event Function

--- ApexVisualizerFunctions/DrivingEventFunctions.cs
+++ ApexVisualizerFunctions/DrivingEventFunctions.cs
@@ -1,6 +1,8 @@
+using ApexCore.DAL.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace ApexVisualizerFunctions
@@ -8,10 +10,14 @@ namespace ApexVisualizerFunctions
     public class DrivingEventFunctions
     {
         private readonly ILogger<DrivingEventFunctions> _logger;
+        private readonly string _connectionString;
+        private readonly ICosmosWriter _cosmosWriter;
 
-        public DrivingEventFunctions(ILogger<DrivingEventFunctions> logger)
+        public DrivingEventFunctions(ILogger<DrivingEventFunctions> logger, IConfiguration configuration, ICosmosWriter cosmosWriter)
         {
             _logger = logger;
+            _cosmosWriter = cosmosWriter;
+            _connectionString = configuration.GetSection("SqlConnection")?.Value ?? throw new ArgumentNullException(nameof(configuration), "Connection string 'SqlConnection' not found.");
         }
 
         [Function("DrivingEventFunctions")]
